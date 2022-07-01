8543cff6d8a3cfd54e11083c8d068ef624357513
oliverester <oliverester@web.de>
2022-07-01 13:27:01 +0000

    adding new paper configs

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from src.exp_management.experiment.SegmentationExperiment import SegmentationExp
 from src.exp_management.run_experiment import run_experiment
 
 if __name__ == '__main__':
-    run_experiment(exp=SegmentationExperiment(config_path='configs_rcc/cv/deeplab_maf_config.yml'))
+    run_experiment(exp=SegmentationExperiment(config_path='configs_paper/configs_rcc/attention/mha/deeplab_res50/d16_k8_deeplab_config.yml'))
  
