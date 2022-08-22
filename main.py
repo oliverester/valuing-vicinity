868081dd1f0b6729bb5391efabee55038bb01ea6
Oliver Ester <oliver.ester@uni-due.de>
2022-08-22 08:57:59 +0000

    avoid illegal arguments

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import sys
+import traceback
 
 from src.exp_management.experiment.SegmentationExperiment import SegmentationExperiment
 from src.exp_management.run_experiment import run_experiment
@@ -16,9 +17,10 @@ if __name__ == '__main__':
     
     conf_parser.add_argument("-c", "--conf_file",
                         help="Specify config file", metavar="FILE")
-    args, _ = conf_parser.parse_known_args()
+    args = conf_parser.parse_args()
     
     try:
         run_experiment(exp=SegmentationExperiment(config_path=args.conf_file))
     except Exception as e:
-        sys.exit(e)                       
+        print(traceback.format_exc())
+        sys.exit(e)                     
